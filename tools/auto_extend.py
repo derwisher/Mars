c23cd2b2347b3afe52a1ea87aa39401b30d50b39
derwisher <00haas@gmail.com>
2025-09-03 22:12:06 +0200

    Update auto_extend.py

--- tools/auto_extend.py
+++ tools/auto_extend.py
@@ -19,7 +19,7 @@ def main(max_n=5000):
     ticks = set(mars.get("active",[]) + mars.get("inactive",[]) +
                 venus.get("active",[]) + venus.get("inactive",[]))
 
-    # # Seeds & Radarlisten (NEU: ex130.txt)
+    # Seeds & Radarlisten (NEU: ex130.txt)
     for fname in ["universe_core.txt","universe_watch.txt","universe_all.txt","extended_universe.txt, "ex130.txt"    # <--- Ex-130 wird automatisch gemerged"]:
         ticks.update(read_lines(DATA/fname))
 
